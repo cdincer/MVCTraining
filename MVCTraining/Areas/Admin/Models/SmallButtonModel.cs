1d036eb8ccc378fb6acd5fea380fd5b8a969667b
cdincer <dincercan@gmail.com>
2018-11-05 21:07:40 +0300

    Index page modification

---
+++ MVCTraining/Areas/Admin/Models/SmallButtonModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Areas.Admin.Models
+{
+    public class SmallButtonModel
+    {
+        public string Action { get; set; }
+        public string Text { get; set; }
+        public string Glyph { get; set; }
+
+    }
+}
