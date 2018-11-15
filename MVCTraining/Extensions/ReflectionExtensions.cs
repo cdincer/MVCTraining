da179238a429f877acf0c6e6ada081e8ccbadc58
Can Dincer <dincercan@gmail.com>
2018-11-15 19:29:30 +0300

    ReflectionExtensions class initialization

---
+++ MVCTraining/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Extensions
+{
+    public class ReflectionExtensions
+    {
+    }
+}
