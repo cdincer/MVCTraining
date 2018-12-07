dd869a6aa43f901e21c77fd3817ff64ed214b5cc
Can Dincer <dincercan@gmail.com>
2018-12-07 23:23:31 +0300

    Product Model Display Name additions

--- MVCTraining/Areas/Admin/Models/ProductModel.cs
+++ MVCTraining/Areas/Admin/Models/ProductModel.cs
@@ -26,8 +26,10 @@ namespace MVCTraining.Areas.Admin.Models
         public int ProductLinkTextId { get; set; }
         public int ProductTypeId { get; set; }
 
+        [DisplayName("Product Link Text")]
         public ICollection<ProductLinkText> ProductLinkTexts { get; set; }
 
+        [DisplayName("Product Type")]
         public ICollection<ProductType> ProductTypes { get; set; }
 
         public string ProductType { get {
