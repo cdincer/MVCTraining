1b1017f71bb4fee485cc5ee6f02f4e3ae515ae15
Can Dincer <dincercan@gmail.com>
2018-12-07 19:45:07 +0300

    Implementation of Product Controller and Product Converter Class

---
+++ MVCTraining/Areas/Admin/Extensions/ConversionExtensions.cs
@@ -0,0 +1,35 @@
+﻿using Memberships.Entities;
+using MVCTraining.Areas.Admin.Models;
+using MVCTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MVCTraining.Areas.Admin.Extensions
+{
+    public static  class ConversionExtensions
+    {
+        public  static async Task<IEnumerable<ProductModel>> Convert(this IEnumerable<Product> products, ApplicationDbContext db)
+        {
+
+            var texts = await db.ProductLinkTexts.ToListAsync();
+            var types = await db.ProductTypes.ToListAsync();
+
+            return from p in products
+                   select new ProductModel
+                   {
+                       Id = p.Id,
+                       Title = p.Title,
+                       Description = p.Description,
+                       ProductLinkTextId = p.ProductLinkTextId,
+                       ProductTypeId = p.ProductTypeId,
+                       ProductLinkTexts = texts,
+                       ProductTypes = types
+
+                   };
+        }
+    }
+}
