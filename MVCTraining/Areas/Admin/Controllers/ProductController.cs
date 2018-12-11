d5bf29f13bfcc11f88d48588473309aeb2f4719d
cdincer <dincercan@gmail.com>
2018-12-11 21:02:50 +0300

    ProductController and its View for Details altered

--- MVCTraining/Areas/Admin/Controllers/ProductController.cs
+++ MVCTraining/Areas/Admin/Controllers/ProductController.cs
@@ -39,7 +39,9 @@ namespace MVCTraining.Areas.Admin.Controllers
             {
                 return HttpNotFound();
             }
-            return View(product);
+            var model = await product.Convert(db);
+            
+            return View(model);
         }
 
         // GET: Admin/Product/Create
