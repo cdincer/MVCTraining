8d673e6901017145d0b918c5d27151e2a00443f1
Can Dincer <dincercan@gmail.com>
2018-12-31 16:37:45 +0300

    SubscriptionProduct Model created

---
+++ MVCTraining/Areas/Admin/Models/SubscriptionProductModel.cs
@@ -0,0 +1,29 @@
+﻿using MVCTraining.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Web;
+using Training.Entities;
+
+namespace MVCTraining.Areas.Admin.Models
+{
+    public class SubscriptionProductModel
+    {
+        [DisplayName("Product Id")]
+        public int ProductId { get; set; }
+
+        [DisplayName("Subscription Id")]
+        public int SubscriptionId { get; set; }
+
+        [DisplayName("Product Title")]
+        public string ProductTitle { get; set; }
+
+        [DisplayName("Subscription Title")]
+        public string SubscriptionTitle { get; set; }
+
+        public ICollection<Product> Products { get; set; }
+        public ICollection<Subscription> Subscriptions { get; set; }
+
+    }
+}
