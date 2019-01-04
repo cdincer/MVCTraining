4e6dce3678e39024e0a134e48c7da59a97b5f2f0
Can Dincer <dincercan@gmail.com>
2019-01-04 21:35:16 +0300

    IdentityExtension class added for user account operations

---
+++ MVCTraining/Extensions/IdentityExtensions.cs
@@ -0,0 +1,24 @@
+﻿using MVCTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+
+namespace MVCTraining.Extensions
+{
+    public static class IdentityExtensions
+    {
+        // user account operations
+
+        public static string GetUserFirstName(this IIdentity identity)
+        {
+            var db =  ApplicationDbContext.Create();
+
+            var user = db.Users.FirstOrDefault(u => u.UserName.Equals(identity.Name));
+
+            return user != null ? user.FirstName : String.Empty;
+        }
+
+    }
+}
