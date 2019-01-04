b8ac22deab9fbf509bd1b024bf5730264e3070e5
Can Dincer <dincercan@gmail.com>
2019-01-04 21:54:34 +0300

    added GetUsers to IdentityExtension

--- MVCTraining/Extensions/IdentityExtensions.cs
+++ MVCTraining/Extensions/IdentityExtensions.cs
@@ -4,6 +4,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.Principal;
 using System.Web;
+using System.Threading.Tasks;
+using System.Data.Entity;
 
 namespace MVCTraining.Extensions
 {
@@ -20,5 +22,17 @@ namespace MVCTraining.Extensions
             return user != null ? user.FirstName : String.Empty;
         }
 
+
+        public static async Task GetUsers(this List<UserViewModel> users)
+        {
+            var db = ApplicationDbContext.Create();
+
+            users.AddRange(await (from u in db.Users
+                                  select new UserViewModel
+                                  { Id = u.Id,
+                                  Email = u.Email,
+                                  FirstName = u.FirstName
+                                  }).OrderBy(o => o.Email).ToListAsync());
+        }
     }
 }
--- MVCTraining/Models/UserViewModel.cs
+++ MVCTraining/Models/UserViewModel.cs
@@ -9,7 +9,7 @@ namespace MVCTraining.Models
     public class UserViewModel
     {
         [Display(Name = "User Id")]
-        public int Id { get; set; }
+        public string Id { get; set; }
 
         [Required]
         [EmailAddress]
