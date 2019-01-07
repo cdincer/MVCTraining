2456300d6f4539754238c98babccd4aa447338c4
cdincer <dincercan@gmail.com>
2019-01-07 21:35:46 +0300

    Account Create Redirection added

--- MVCTraining/Controllers/AccountController.cs
+++ MVCTraining/Controllers/AccountController.cs
@@ -512,7 +512,6 @@ namespace MVCTraining.Controllers
                 var result = await UserManager.CreateAsync(user, model.Password);
                 if (result.Succeeded)
                 {
-                    await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
 
                     // For more information on how to enable account confirmation and password reset please visit http://go.microsoft.com/fwlink/?LinkID=320771
                     // Send an email with this link
