f3ed345433923b20d35f6ffcd777777b584e1a72
cdincer <dincercan@gmail.com>
2019-01-09 21:13:00 +0300

    User Subscription related models created

---
+++ MVCTraining/Models/UserSubscriptionViewModel.cs
@@ -0,0 +1,18 @@
+﻿using MVCTraining.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Models
+{
+    public class UserSubscriptionViewModel
+    {
+        public ICollection<Subscription> Subscriptions { get; set; }
+        public ICollection<UserSubscriptionModel> UserSubscriptions { get; set; }
+        public bool DisableDropDown { get; set; }
+        public string UserId { get; set; }
+        public int SubscriptionId { get; set; }
+
+    }
+}
