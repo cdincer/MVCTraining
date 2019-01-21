9da7f5f3d22f251cd80b5a80f1243e410e6b9060
cdincer <dincercan@gmail.com>
2019-01-21 21:16:43 +0300

    Thumbnail Comparing Interface added

---
+++ MVCTraining/Comparers/ThumbnailEqualityComparer.cs
@@ -0,0 +1,21 @@
+﻿using MVCTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Comparers
+{
+    public class ThumbnailEqualityComparer : IEqualityComparer<ThumbnailModel>
+    {
+        public bool Equals(ThumbnailModel thumb1, ThumbnailModel thumb2)
+        {
+            return thumb1.ProductId.Equals(thumb2.ProductId);
+        }
+
+        public int GetHashCode(ThumbnailModel obj)
+        {
+            return obj.ProductId;
+        }
+    }
+}
