2b11ee189e5a44dd4a1fae235307c04ed6dc5aef
cdincer <dincercan@gmail.com>
2019-01-21 21:08:51 +0300

    UserSubscription getting extension added

---
+++ MVCTraining/Extensions/ThumbnailExtensions.cs
@@ -0,0 +1,38 @@
+﻿using MVCTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace MVCTraining.Extensions
+{
+    public static class ThumbnailExtensions
+    {
+        private static async Task<List<int>> GetSubscriptionIdAsync(string userId = null, ApplicationDbContext db = null)
+        {
+            try
+            {
+                if (userId == null) return new List<int>();
+                if (db == null) db = ApplicationDbContext.Create();
+
+
+                return await (
+                    from us in db.UserSubscriptions
+                    where us.UserId.Equals(userId)
+                    select us.SubscriptionId).ToListAsync();
+                
+                
+            }
+            catch
+            {
+
+
+            }
+
+            return new List<int>();
+
+        }
+    }
+}
