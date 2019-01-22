222160571168aea08f4f8b3c3487a65f439015aa
Can Dincer <dincercan@gmail.com>
2019-01-22 19:40:26 +0300

    ProductThumbnail getting method added

--- MVCTraining/Extensions/ThumbnailExtensions.cs
+++ MVCTraining/Extensions/ThumbnailExtensions.cs
@@ -1,5 +1,7 @@
-﻿using MVCTraining.Models;
+﻿using MVCTraining.Comparers;
+using MVCTraining.Models;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
@@ -22,8 +24,8 @@ namespace MVCTraining.Extensions
                     from us in db.UserSubscriptions
                     where us.UserId.Equals(userId)
                     select us.SubscriptionId).ToListAsync();
-                
-                
+
+
             }
             catch
             {
@@ -34,5 +36,50 @@ namespace MVCTraining.Extensions
             return new List<int>();
 
         }
+
+
+        public static async Task<IEnumerable<ThumbnailModel>> GetProductThumbnailsAsync(this List<ThumbnailModel> thumbnails, string userId = null, ApplicationDbContext db = null)
+        {
+
+            try
+            {
+
+                if (userId == null) return new List<ThumbnailModel>();
+                if (db == null) db = ApplicationDbContext.Create();
+
+                var subscriptionIds = await GetSubscriptionIdAsync(userId, db);
+
+                thumbnails = await (from ps in db.SubscriptionProducts
+                                    join p in db.Products on ps.ProductId equals p.Id
+                                    join plt in db.ProductLinkTexts on p.ProductLinkTextId equals plt.Id
+                                    join pt in db.ProductTypes on p.ProductTypeId equals pt.Id
+                                    where subscriptionIds.Contains(ps.SubscriptionId)
+                                    select new ThumbnailModel
+                                    {
+                                        ProductId = p.Id,
+                                        SubscriptionId = ps.SubscriptionId,
+                                        Description = p.Description,
+                                        ImageUrl = p.ImageUrl,
+                                        Link = "/ProductContent/Index/" + p.Id,
+                                        TagText = plt.Title,
+                                        ContentTag = pt.Title,
+
+                                    }).ToListAsync();
+
+
+
+
+            }
+
+            catch
+            {
+
+            }
+
+            return thumbnails.Distinct( new ThumbnailEqualityComparer()).OrderBy(o => o.Title);
+
+        }
+
     }
+
 }
