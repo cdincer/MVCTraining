feeba8304ebf0a75d41d84ea3ab8b953c69cb874
Can Dincer <dincercan@gmail.com>
2019-01-25 22:27:29 +0300

    Added ProductSectionEqualityComparer

---
+++ MVCTraining/Comparers/ProductSectionEqualityComparer.cs
@@ -0,0 +1,22 @@
+﻿using MVCTraining.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Comparers
+{
+    public class ProductSectionEqualityComparer : IEqualityComparer<ProductSection>
+    {
+        public bool Equals(ProductSection section1, ProductSection section2)
+        {
+
+            return section1.Id.Equals(section2.Id);
+        }
+
+        public int GetHashCode(ProductSection section)
+        {
+            return (section.Id).GetHashCode();
+        }
+    }
+}
