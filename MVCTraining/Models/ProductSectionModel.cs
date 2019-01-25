a9ac8d0d3b203a9cc755cd71b45dd2396a777c92
Can Dincer <dincercan@gmail.com>
2019-01-25 22:29:23 +0300

    Adding ViewModel  (ProductSectionModel) for Product Section Index

---
+++ MVCTraining/Models/ProductSectionModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCTraining.Models
+{
+    public class ProductSectionModel
+    {
+        public string Title { get; set; }
+        public List<ProductSection> Sections { get; set; }
+
+    }
+}
