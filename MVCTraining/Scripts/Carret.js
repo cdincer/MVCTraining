4df31441a23b843ca60f8e9b002616c0394e547c
Can Dincer <dincercan@gmail.com>
2019-02-05 19:50:46 +0300

    Rotation with js added

---
+++ MVCTraining/Scripts/Carret.js
@@ -0,0 +1,9 @@
+﻿$(function ()
+{
+    $(".panel-carret").click(function (e)
+    {
+        $(this).toggleClass("pressed");
+        $(this).children("glyphicon-play").toggleClass("gly-rotate-90");
+        e.preventDefault();
+    });
+});
