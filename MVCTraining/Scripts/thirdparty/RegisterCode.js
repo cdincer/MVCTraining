f4f5a3ea2cec3847b1cb09685017a8e6c158a6d1
cdincer <dincercan@gmail.com>
2019-02-14 20:42:27 +0300

    Registercode javascript message functions added

--- MVCTraining/Scripts/thirdparty/RegisterCode.js
+++ MVCTraining/Scripts/thirdparty/RegisterCode.js
@@ -1,1 +1,45 @@
-﻿
+﻿$(function ()
+{
+    var code = $(".register-code-panel input");
+    
+    
+    function displayMessage(success, message)
+    {
+        var alert_div = $(".register-code-panel .alert");
+        alert_div.text(message);
+
+        if (success)
+            alert_div.removeClass('alert-danger').addClass('alert-success');
+        else
+            alert_div.removeClass('alert-success').addClass('alert-danger');
+
+        alert_div.removeClass('hidden');
+
+    }
+
+    
+    $(".register-code-panel button").click(function (e)
+    {
+        $(".register-code-panel .alert").addClass('hidden');
+
+        if (code.val().length == 0)
+        {
+            displayMessage(false, "Enter a code");
+
+            return;
+        }
+
+        $.post('/RegisterCode/Register', { code: code.val() },
+        function(data)
+        {
+            displayMessage(true, "The code was succesfully added. \n\r Please reload the page.");
+            code.val('');
+        }).fail(function(xlr,status,error){
+            displayMessage(false, "Could not register the code");
+        });
+          
+      
+    });
+
+
+});
