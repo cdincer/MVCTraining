adc261c577a4856ac25e29218f96113221c9af54
cdincer <dincercan@gmail.com>
2019-02-14 20:15:20 +0300

    Register method and its javascript file added

--- MVCTraining/Controllers/RegisterCodeController.cs
+++ MVCTraining/Controllers/RegisterCodeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MVCTraining.Extensions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -10,10 +11,27 @@ namespace MVCTraining.Controllers
     public class RegisterCodeController : Controller
     {
         // GET: RegisterCode
-        public async Task<ActionResult> Register()
+        public async Task<ActionResult> Register(string code)
         {
 
+            if (Request.IsAuthenticated)
+            {
+                var userId = HttpContext.GetUserId();
+
+                var registered = await SubscriptionExtensions.RegisterUserSubscriptionCode(userId,code);
+                if (!registered)
+                {
+                    throw new ApplicationException();
+                }
+
+                return PartialView("_RegisterCodePartial");
+
+            }
             return View();
         }
+
+
+
+
     }
 }
