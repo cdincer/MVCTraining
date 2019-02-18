cde8f548a9a98ae95dfbd1ecb47e7f347cabe3ff
Can Dincer <dincercan@gmail.com>
2019-02-18 20:35:52 +0300

    setting RegisterUser.js for user register partial

--- MVCTraining/Scripts/RegisterUser.js
+++ MVCTraining/Scripts/RegisterUser.js
@@ -8,4 +8,26 @@
         $('.register-user-panel button').toggleClass('disabled');
     }
 
+
+    var registerUserButton = $('.register-user-panel button').click(
+        onRegisterUserClick);
+
+    function onRegisterUserClick()
+    {
+        var url = '/Account/RegisterUserAsync';
+        var AntiForgeryToken = $('[name="__RequestVerificationToken"]').val();
+
+        var name = $('register-user-panel .first-name').val();
+        var email = $('register-user-panel .email').val();
+        var pwd = $('register-user-panel .password').val();
+
+        $.post(url, {
+            __RequestVerificationToken: AntiForgeryToken, email: email, name: name, password: pwd,acceptUserAgreement: true}, 
+        function (data)
+        {
+            alert('Post succesful');
+        }).fail(function (xhr, status, error) { alert('Post unsuccesful'); })
+    
+    
+    }
 });
