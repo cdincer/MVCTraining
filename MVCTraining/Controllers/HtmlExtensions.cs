e348b3ae52f4015de8e6ef4757fe3e8293e72fb7
Can Dincer <dincercan@gmail.com>
2019-02-19 20:07:59 +0300

    HtmlExtension class/method added

---
+++ MVCTraining/Controllers/HtmlExtensions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCTraining.Controllers
+{
+    public static class HtmlExtensions
+    {
+        public static MvcHtmlString GlypgLink(this HtmlHelper htmlHelper,string controller,string action,string text, string glyphicon,string cssClasses="",string id="")
+        {
+            //declare a span for the glyphicon
+            var glyph = string.Format("<span class='glyphicon glyphicon-{0}></span>'", glyphicon);
+
+
+            //declare the anchor tag
+            var anchor = new TagBuilder("a");
+            anchor.MergeAttribute("href",string.Format("/{0}/{1}/",controller,action));
+
+            anchor.InnerHtml = string.Format("{0} {1}", glyph, text);
+            anchor.AddCssClass(cssClasses);
+            anchor.GenerateId(id);
+
+
+            return MvcHtmlString.Create(anchor.ToString(TagRenderMode.Normal));
+        }
+    }
+}
