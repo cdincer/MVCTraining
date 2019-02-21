e9ac92293fdb1b0ee1abb9185a75441498e7fc92
Can Dincer <dincercan@gmail.com>
2019-02-21 20:02:16 +0300

    Styling login partial

--- MVCTraining/Scripts/Login.js
+++ MVCTraining/Scripts/Login.js
@@ -1,1 +1,18 @@
-﻿
+﻿$(function () {
+    var loginLinkHover = $('#loginLink').hover(onLoginLinkHover);
+    var loginCloseButton = $('#close-login').click(onCloseLogin);
+
+    function onLoginLinkHover()
+    {
+            $("div[data-login-user-area]").addClass('open');
+    }
+
+    function onCloseLogin()
+    {
+        $("div[data-login-user-area]").removeClass('open');
+
+
+    }
+    
+
+});
