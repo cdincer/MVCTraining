07a466279dba85c201ee0204df1f4ab8e5742bad
Can Dincer <dincercan@gmail.com>
2019-02-25 19:59:00 +0300

    Improving link method

--- MVCTraining/Extensions/HtmlExtensions.cs
+++ MVCTraining/Extensions/HtmlExtensions.cs
@@ -8,7 +8,8 @@ namespace MVCTraining.Extensions
 {
     public static class HtmlExtensions
     {
-        public static MvcHtmlString GlyphLink(this HtmlHelper htmlHelper,string controller,string action,string text, string glyphicon,string cssClasses="",string id="")
+        public static MvcHtmlString GlyphLink(this HtmlHelper htmlHelper,string controller,string action,string text, string glyphicon,string cssClasses="",string id="",
+            Dictionary<string,string> attributes = null)
         {
             //declare a span for the glyphicon
             var glyph = string.Format("<span class='glyphicon glyphicon-{0}'></span>", glyphicon);
@@ -16,7 +17,16 @@ namespace MVCTraining.Extensions
 
             //declare the anchor tag
             var anchor = new TagBuilder("a");
-            anchor.MergeAttribute("href",string.Format("/{0}/{1}/",controller,action));
+            if (controller.Length > 0)
+                anchor.MergeAttribute("href", string.Format("/{0}/{1}/", controller, action));
+            else
+                anchor.MergeAttribute("href", "#");
+
+            if (attributes != null)
+                foreach (var attribute in attributes)
+                    anchor.MergeAttribute(attribute.Key, attribute.Value);
+
+
 
             anchor.InnerHtml = string.Format("{0} {1}", glyph, text);
             anchor.AddCssClass(cssClasses);
