dfbf6e0658ee28c7486d3cb6454584e2b72a0cce
cdincer <dincercan@gmail.com>
2019-02-26 20:14:50 +0300

    Forgot-password javascript beginning

--- MVCTraining/Scripts/forgot-password.js
+++ MVCTraining/Scripts/forgot-password.js
@@ -1,1 +1,28 @@
-﻿
+﻿$(function()
+{
+    var pwdLinkHover = $('#pwdLink').hover(onCloseLogin);
+    var pwdLinkHover = $('#resetPwd').click(onResetpassword);
+
+
+    function onCloseLogin()
+    {
+        $('div[data-login-user-area]').removeClass('open');
+
+    }
+
+    function onResetpassword()
+    {
+        var email = $('.modal-dialog .reset-email').val();
+        var antiforgery = $('[name="__RequestVerificationToken"]').val();
+        var url = "/Account/ForgotPasswordConfirmation";
+
+        //alert(email + " " + antiforgery + " " + url);
+
+        $.post("/Account/ForgotPassword", new {__RequestVerificationToken:antiforgery,email: email},
+            function (data)
+            { 
+                location.href = url;
+            }).fail(function xhr,status,error){ location.href=url; });
+    }
+
+});
